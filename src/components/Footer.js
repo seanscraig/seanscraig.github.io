1cfc3fe9007116cd43f4e420b890cdcb33444393
Sean Craig <seancraig21@gmail.com>
2021-12-02 19:40:11 -0600

    added icons and fixed footer position

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
     <div className="footer">
-      <a href="tel:651-398-6729">(651) 398-6729</a>
-      <a href="mailto: seancraig21@gmail.com">seancraig21@gmail.com</a>
-      <a href="https://github.com/seanscraig">GitHub</a>
-      <a href="https://www.linkedin.com/in/sean-craig-bb219861/">Linkedin</a>
+      <a href="tel:651-398-6729"><i class="fas fa-phone-square fa-2x"></i></a>
+      <a href="mailto: seancraig21@gmail.com"><i class="fas fa-envelope-square fa-2x"></i></a>
+      <a href="https://github.com/seanscraig"><i class="fab fa-github fa-2x"></i></a>
+      <a href="https://www.linkedin.com/in/sean-craig-bb219861/"><i class="fab fa-linkedin fa-2x"></i></a>
     </div>
   );
 };
