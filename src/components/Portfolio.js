239af4c8f19b67cdb44f63b5f14ebd397d716198
Sean Craig <seancraig21@gmail.com>
2022-01-12 10:36:19 -0600

    flxed portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,45 +1,76 @@
-import image1 from "../assets/images/project-2-screen-shot.png";
-import image2 from "../assets/images/tech-blog-screen-shot.png";
-import image3 from "../assets/images/note-taker-screen-shot.png";
-import image4 from "../assets/images/project-1-screen-shot.png";
-import image5 from "../assets/images/weather-dashboard.png";
-
+import image1 from "../assets/images/book-search-engine.png";
+import image2 from "../assets/images/pwa-budget-tracker.png";
+import image3 from "../assets/images/workout-tracker.png";
+import image4 from "../assets/images/project-2-screen-shot.png";
+import image5 from "../assets/images/tech-blog-screen-shot.png";
+import image6 from "../assets/images/note-taker-screen-shot.png";
+import image7 from "../assets/images/project-1-screen-shot.png";
+import image8 from "../assets/images/weather-dashboard.png";
+import image9 from "../assets/images/day-planner.png";
 const Portfolio = () => {
   const portfolioItems = [
     {
       id: 1,
-      title: "Video Game Database",
-      tech: "Node.js/Sequelize/Express/Handlebars",
+      title: "Book Search Engine",
+      tech: "MERN Stack",
       image: image1,
-      link: "https://vast-lake-35863.herokuapp.com/",
+      link: "https://sean-book-search.herokuapp.com/",
     },
     {
       id: 2,
-      title: "Tech Blog",
-      tech: "Node.js/Sequelize/Express/Handlebars",
+      title: "PWA Budget Tracker",
+      tech: "MERN Stack",
       image: image2,
-      link: "http://sean-tech-blog.herokuapp.com/",
+      link: "https://sean-pwa-budget-tracker.herokuapp.com/",
     },
     {
       id: 3,
-      title: "Note Taker",
-      tech: "Node.js/Express/JavaScript/HTML/CSS",
+      title: "Workout Tracker",
+      tech: "MERN Stack",
       image: image3,
-      link: "https://stark-ridge-83519.herokuapp.com",
+      link: "https://sean-workout-tracker.herokuapp.com/",
     },
     {
       id: 4,
-      title: "Nonprofit Career Planner",
-      tech: "JavaScript/ProPublica API/HTML/CSS",
+      title: "Video Game Database",
+      tech: "Node.js/Sequelize/Express/Handlebars",
       image: image4,
-      link: "https://sthompsonchicago.github.io/Nonprofit-Career-Planner/",
+      link: "https://vast-lake-35863.herokuapp.com/",
     },
     {
       id: 5,
+      title: "Tech Blog",
+      tech: "Node.js/Sequelize/Express/Handlebars",
+      image: image5,
+      link: "http://sean-tech-blog.herokuapp.com/",
+    },
+    {
+      id: 6,
+      title: "Note Taker",
+      tech: "Node.js/Express/JavaScript/HTML/CSS",
+      image: image6,
+      link: "https://stark-ridge-83519.herokuapp.com",
+    },
+    {
+      id: 7,
+      title: "Nonprofit Career Planner",
+      tech: "JavaScript/ProPublica API/HTML/CSS",
+      image: image7,
+      link: "https://sthompsonchicago.github.io/Nonprofit-Career-Planner/",
+    },
+    {
+      id: 8,
       title: "Weather Dashboard",
       tech: "JavaScript/HTML/CSS",
-      image: image5,
+      image: image8,
       link: "https://seanscraig.github.io/weather-dashboard/",
+    },
+    {
+      id: 9,
+      title: "Day Planner",
+      tech: "JavaScript/HTML/CSS",
+      image: image9,
+      link: "https://seancraig.me/day-planner/",
     },
   ];
 
